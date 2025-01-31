91f16ae4042ac581a4ab7062abc0e664fcaa0af0
Warren MacEvoy <wmacevoy@gmail.com>
2025-01-31 11:51:53 -0700

    game example

---
+++ game/game.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main() {
+   double prob = 1, player1 = 1, player2 = 1;
+   int turns = 11;
+   printf("prob,player1,player2,pid\n");
+   for (int turn = 0; turn < turns; ++turn) {
+      if (fork() != 0) {
+        double winnings = 0.5*player1;
+        prob *= 0.6;
+        player1 -= winnings;
+        player2 += winnings;
+      } else {
+        double winnings = 0.5*player2;
+        prob *= 0.4;
+        player1 += winnings;
+        player2 -= winnings;
+      }
+   }
+   printf("%lg,%lg,%lg,%d\n",prob,player1,player2,getpid());
+   return 0;
+}
